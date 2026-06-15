d810ec7a126ac9d0e01b4b98ad266e9bcdc8362f
Ibrahim AboElsoud <ibrahimaboelsoud44@gmail.com>
2026-06-15 18:02:55 +0300

    Update server.js

--- server.js
+++ server.js
@@ -19,7 +19,13 @@ const app = express();
 
 // Middleware
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
+app.use(
+  express.urlencoded({
+    limit: "10mb",
+    extended: true,
+  })
+);
 app.use(cookieParser());
 app.use(helmet());
 
@@ -34,6 +40,7 @@ app.use(limiter);
 app.use("/api/auth", require("./src/routes/authRoutes"));
 app.use("/api/items", require("./src/routes/itemRoutes"));
 app.use("/api/wardrobe", require("./src/routes/wardrobeRoutes"));
+app.use("/api/chat", require("./src/routes/chatRoutes"));
 // Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
