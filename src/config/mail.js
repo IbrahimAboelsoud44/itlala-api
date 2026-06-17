d92c3212ffdf6bbcdd61f1b548087fd58e669914
Ibrahim AboElsoud <ibrahimaboelsoud44@gmail.com>
2026-06-17 15:58:42 +0300

    Update mail.js

--- src/config/mail.js
+++ src/config/mail.js
@@ -8,6 +8,7 @@ const transporter = nodemailer.createTransport({
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  connectionTimeout: 30000,
+  greetingTimeout: 30000,
+  socketTimeout: 30000,
 });
-
-module.exports = transporter;
