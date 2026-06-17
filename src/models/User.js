b0c005e998e589b4c3ea5d07a94aefcbc4bf3442
Ibrahim AboElsoud <ibrahimaboelsoud44@gmail.com>
2026-06-17 04:48:09 +0300

    Update User.js

--- src/models/User.js
+++ src/models/User.js
@@ -24,7 +24,7 @@ const userSchema = new mongoose.Schema(
     },
     provider: {
       type: String,
-      default: "local", // local / google
+      default: "local", // local / google / facebook
     },
     role: {
       type: String,
